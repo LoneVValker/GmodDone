fc96d5a89ffe209b811a2eb42f7119d1aa3d4a5a
LoneVValker <32742283+LoneVValker@users.noreply.github.com>
2017-10-28 16:31:59 +0300

    Update player.cpp

--- source/player.cpp
+++ source/player.cpp
@@ -4,17 +4,11 @@ using namespace Main;
 
 /*
  FL_IN_AIR_STAND                    256
- FL_ON_GROUND                     257
  FL_IN_AIR_MOVING_TO_STAND         258
- FL_ON_GROUND_MOVING_TO_STAND     259
  FL_IN_AIR_MOVING_TO_CROUCH         260
- FL_ON_GROUND_MOVING_TO_CROUCH     261
  FL_IN_AIR_CROUCHED                 262
- FL_ON_GROUND_CROUCHED             263
- FL_IN_WATER                     1280
- FL_IN_PUDDLE                     1281
- FL_IN_WATER_CROUCHED             1286
- FL_IN_PUDDLE_CROUCHED             1287
+ FL_IN_AIR_PUDDLE                     1280
+ FL_IN_AIR_PUDDLE_CROUCHED             1282
  */
 
 int p_base;
@@ -37,6 +31,6 @@ void Player::start() {
 }
 
 bool Player::in_air() {
-	return flag == 256 || flag == 258 || flag == 260 || flag == 262 || flag == 1281 || flag == 1287;
+	return flag == 256 || flag == 258 || flag == 260 || flag == 262 || flag == 1280 || flag == 1282;
 }
 
